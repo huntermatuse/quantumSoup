ef8fe8892b7144f089b960ff5dc5e3cdec502063
Hunter Matuse <93264681+huntermatuse@users.noreply.github.com>
2024-09-03 21:41:26 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="quantumSoup",
-    version="1.0.0",
+    version="0.1.0",
     author="Hunter Matuse",
     author_email="hmatuse@advancingsimple.com",
     description="A Python API to Canary Lab's historian web services.",
@@ -15,7 +15,13 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
